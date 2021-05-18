3484bc3eb55f43c5ed7a72d4a8b64cb2d0a21dd8
Nghia <bhtv28@gmail.com>
2021-05-18 10:22:55 +0700

    try to use handler struct to replace handlerfunc

--- main.go
+++ main.go
@@ -5,28 +5,28 @@ import (
 	"net/http"
 )
 
-type MyHandler struct{}
+type SecretHandler struct{}
 
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-}
-
-func secretHandler(writer http.ResponseWriter, request *http.Request) {
+func (h *SecretHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "secret handler")
 }
 
-func healthCheckHandler(writer http.ResponseWriter, request *http.Request) {
+type HealthCheckHandler struct{}
+
+func (h *HealthCheckHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "healthcheck")
 }
 
 func main() {
-	http.HandleFunc("/", secretHandler)
-	http.HandleFunc("/healthcheck", healthCheckHandler)
+	secretHandler := SecretHandler{}
+	healthCheckHandler := HealthCheckHandler{}
 
-	handler := MyHandler{}
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: &handler,
+		Addr: "127.0.0.1:8080",
 	}
+
+	http.Handle("/secret", &secretHandler)
+	http.Handle("/healthcheck", &healthCheckHandler)
+
 	server.ListenAndServe()
 }
