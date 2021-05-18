02c4406ceac239d9b84914aca0d2d74d56963a53
Nghia <bhtv28@gmail.com>
2021-05-18 10:04:34 +0700

    use handler struct with ServeHTTP

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
 func secretHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "secret handler")
 }
@@ -17,9 +23,10 @@ func main() {
 	http.HandleFunc("/", secretHandler)
 	http.HandleFunc("/healthcheck", healthCheckHandler)
 
+	handler := MyHandler{}
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
-		Handler: nil,
+		Handler: &handler,
 	}
 	server.ListenAndServe()
 }
