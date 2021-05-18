16d6cf395827afc39cdeb9d761c04d8c1965a430
Nghia <bhtv28@gmail.com>
2021-05-18 21:11:47 +0700

    try cookie, json handler

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
+	"time"
 )
 
 type SecretHandler struct{}
@@ -11,22 +16,115 @@ func (h *SecretHandler) ServeHTTP(writer http.ResponseWriter, request *http.Requ
 	fmt.Fprintf(writer, "secret handler")
 }
 
-type HealthCheckHandler struct{}
-
-func (h *HealthCheckHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func healthcheck(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "healthcheck")
+}
+
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called - ", name)
+		h(w, r)
+	}
+}
+
+func protect(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Handler is protected")
+		h(w, r)
+	}
+}
+
+func body(w http.ResponseWriter, r *http.Request) {
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	fmt.Fprintf(w, string(body))
+}
+
+type Post struct {
+	User    string
+	Threads []string
+}
+
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "Sau Sheong",
+		Threads: []string{"first", "second", "third"},
+	}
+	json, _ := json.Marshal(post)
+	w.Write(json)
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co",
+		HttpOnly: true,
+	}
+	w.Header().Set("Set-Cookie", c1.String())
+	w.Header().Add("Set-Cookie", c2.String())
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+	cs := r.Cookies()
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
+}
+
+func setMessage(w http.ResponseWriter, r *http.Request) {
+	msg := []byte("Hello World!")
+	c := http.Cookie{
+		Name:  "flash",
+		Value: base64.URLEncoding.EncodeToString(msg),
+	}
+	http.SetCookie(w, &c)
+}
+
+func showMessage(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("flash")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			fmt.Fprintln(w, "No message found")
+		}
+
+	} else {
+		rc := http.Cookie{
+			Name:    "flash",
+			MaxAge:  -1,
+			Expires: time.Unix(1, 0),
+		}
+		http.SetCookie(w, &rc)
+		val, _ := base64.URLEncoding.DecodeString(c.Value)
+		fmt.Fprintln(w, string(val))
+	}
 }
 
 func main() {
 	secretHandler := SecretHandler{}
-	healthCheckHandler := HealthCheckHandler{}
 
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
 	http.Handle("/secret", &secretHandler)
-	http.Handle("/healthcheck", &healthCheckHandler)
+	http.HandleFunc("/healthcheck", log(healthcheck))
+	http.HandleFunc("/body", body)
+	http.HandleFunc("/json", jsonExample)
+	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
+	http.HandleFunc("/set_message", setMessage)
+	http.HandleFunc("/show_message", showMessage)
 
 	server.ListenAndServe()
 }
